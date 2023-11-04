b2b3b5e0c77dfb615b86a532fe8f8770b305a329
Minou <118366099+Snanskl@users.noreply.github.com>
2023-11-04 22:34:18 +0100

    Add files via upload

---
+++ Assignment 2/Assignment 2/MainProgram.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment_2
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Strings, selection and iteration in C#";
+            FunFeatures funfeatureObj = new FunFeatures();
+            funfeatureObj.Start();
+
+            Console.Title = "Let's work with numbers!";
+            MathWork mathObj = new MathWork();
+            mathObj.Start();
+
+            Console.Title = "Let's convert celsius and farenheit!";
+            TemperatureConverter temperatureConverter = new TemperatureConverter();
+            temperatureConverter.Start();
+
+            WorkingSchedule workingObjct = new WorkingSchedule();
+            workingObjct.Start();
+        }
+    }
+}
